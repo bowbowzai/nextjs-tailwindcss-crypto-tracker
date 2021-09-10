67e151afb9f5a8dfb21e6470e701326920f7b9ff
bowbowzai <jupeng2015@gmail.com>
2021-09-10 17:33:55 +0800

    update

--- components/Item/Item.jsx
+++ components/Item/Item.jsx
@@ -11,8 +11,8 @@ export default function Item({
 }) {
   return (
     <Link href={`/coin/${id}`}>
-      <a href="">
-        <div className="grid grid-cols-3 lg:grid-cols-6 grid-rows-1 text-black gap-x-1 lg:gap-x-10 border-b-1 border-gray-400 items-center text-center lg:text-left text-white py-3 px-2 hover:bg-gray-800">
+      <a href="" className="text-white">
+        <div className="grid grid-cols-3 lg:grid-cols-6 grid-rows-1 gap-x-1 lg:gap-x-10 border-b-1 border-gray-400 items-center text-center lg:text-left text-white py-3 px-2 hover:bg-gray-800">
           <div className="flex justify-start items-center">
             <div className="w-6 h-6 mr-1.5">
               <img src={image} alt="" className="w-full align-middle h-full" />
